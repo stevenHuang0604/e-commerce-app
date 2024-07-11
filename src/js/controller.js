ea9d69527a0b1807972afaf7b0daa1540a39b217
Steven Huang <sarsky228@gmail.com>
2024-07-11 18:02:51 +0800

    feat: add main preview html structure

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,8 @@
 import { getAllProducts } from './api.js';
+import { API_URL } from './config.js';
 
 const getSingleProduct = async () => {
-  const res = await fetch('https://dummyjson.com/products/search?q=hat');
+  const res = await fetch('https://dummyjson.com/products/search?q=shoes');
   const data = await res.json();
   console.log(data);
 };

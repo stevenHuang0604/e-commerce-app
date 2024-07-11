73fd3ff8187191e077acbcfc721bde13e5e595d8
Steven Huang <sarsky228@gmail.com>
2024-07-11 14:49:06 +0800

    feat: add filter html stucture

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 import { getAllProducts } from './api.js';
 
 const getSingleProduct = async () => {
-  const res = await fetch('https://dummyjson.com/products/search?q=shoes');
+  const res = await fetch('https://dummyjson.com/products/search?q=hat');
   const data = await res.json();
   console.log(data);
 };
